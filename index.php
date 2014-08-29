986086af0135b1b7e45a502d0785ab1273bb780b
Tom Henderson <tomhenderson@mac.com>
2014-08-29 15:42:56 +1200

    Add dummy index page.

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ): ?>
-  <?php while ( have_posts() ) : the_post(); ?>
-    <h2>
-      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-    </h2>
-    <?php the_excerpt(); ?>
-  <?php endwhile; wp_reset_query(); ?>
-<?php else: ?>
-  <h2>No posts found</h2>
-<?php endif; ?>
-
-<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-  <div class="prev">
-    <?php next_posts_link( __( '&larr; Older posts' ) ); ?>
+<div class="wrapper">
+  <div class="content home">
+    <ul>
+      <li><a href="#"><img src="images/menu/home-1.jpg"><span class="menu-title">Drinks<br />List</span></a></li>
+      <li><a href="#"><img src="images/menu/home-2.jpg"><span class="menu-title">Online<br />Reservations</span></a></li>
+      <li><a href="#"><img src="images/menu/home-3.jpg"><span class="menu-title">Upcoming<br />Events</span></a></li>
+    </ul>
   </div>
-  <div class="next">
-    <?php previous_posts_link( __( 'Newer posts &rarr;' ) ); ?>
-  </div>
-<?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
