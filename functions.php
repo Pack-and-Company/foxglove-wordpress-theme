d9408626c13574bbc9c47e6eda38e74e07bbacdd
Tom Henderson <tomhenderson@mac.com>
2014-08-29 14:32:13 +1200

    Testing.

--- functions.php
+++ functions.php
@@ -13,16 +13,18 @@ function my_init_method() {
 add_action('init', 'my_init_method');
 
 function the_content_before_more() {
+	global $post;
 	$more_string = '<!--more-->';
-	return $page_content = explode($more_string, get_the_content());
+	return $page_content = explode($more_string, $post->post_content);
 	$page_content[0] = apply_filters('the_content', $page_content[0]);
 	$page_content[0] = str_replace(']]>', ']]&gt;', $page_content[0]);
 	# return $page_content[0];
 }
 
 function the_content_after_more() {
+	global $post;
 	$more_string = '<!--more-->';
-	$page_content = explode($more_string, get_the_content());
+	$page_content = explode($more_string, $post->post_content);
 	$page_content[1] = apply_filters('the_content', $page_content[1]);
 	$page_content[1] = str_replace(']]>', ']]&gt;', $page_content[1]);
 	return $page_content[1];
