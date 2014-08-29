40c4477d957e872b6bb12af42af703ed48312efa
Tom Henderson <tomhenderson@mac.com>
2014-08-29 15:55:45 +1200

    Add placeholders.

--- page-home.php
+++ page-home.php
@@ -11,9 +11,9 @@ Template Name: Home Page
 <div class="wrapper">
   <div class="content home">
     <ul>
-      <li><a href="#"><img src="images/menu/home-1.jpg"><span class="menu-title">Drinks<br />List</span></a></li>
-      <li><a href="#"><img src="images/menu/home-2.jpg"><span class="menu-title">Online<br />Reservations</span></a></li>
-      <li><a href="#"><img src="images/menu/home-3.jpg"><span class="menu-title">Upcoming<br />Events</span></a></li>
+      <li><a href="#"><img src="http://placehold.it/300x204"><span class="menu-title">Drinks<br />List</span></a></li>
+      <li><a href="#"><img src="http://placehold.it/300x204"><span class="menu-title">Online<br />Reservations</span></a></li>
+      <li><a href="#"><img src="http://placehold.it/300x204"><span class="menu-title">Upcoming<br />Events</span></a></li>
     </ul>
   </div>
 </div>
