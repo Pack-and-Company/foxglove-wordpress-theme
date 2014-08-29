72a6d18b151205604bf9a8c74424f4ceb099ee5a
Tom Henderson <tomhenderson@mac.com>
2014-08-29 13:27:41 +1200

    Add scripts.

---
+++ scripts/scripts.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+	$('.dark-toggle').click(function() {
+		$('body').toggleClass('dark');
+
+		if ( $('body').hasClass('dark') ) {
+			$('.featured-header').attr("src", "images/foxglove/header-foxtail.jpg");
+			$('.logo').attr("src", "images/foxglove/logo-foxtail.png");
+		} else {
+			$('.featured-header').attr("src", "images/snapdragon/header-about.jpg");
+			$('.logo').attr("src", "images/snapdragon/logo-snapdragon.png");
+		}
+	});
+
+	if ( $('.nav .selected').length ) {
+		var home_width = $('.nav .selected a').width();
+		var home_position = $('.nav .selected a').position().left + 10;
+		$('.nav .selected').removeClass('selected');
+		$('.underline').stop(false, false).animate({ left: home_position, width: home_width }, 300);
+	} else {
+		var home_width = 0;
+		var home_position = 60;
+	}
+
+	$('.nav .item').hover(function(){
+		var width = $(this).width();
+		var position = $(this).position().left + 10;
+		$('.underline').stop(false, false).animate({ left: position, width: width }, 300);
+	}, function(){
+		$('.underline').stop(false, false).animate({ left: home_position, width: home_width }, 300);
+	});
+});
