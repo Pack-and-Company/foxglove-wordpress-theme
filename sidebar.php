c2c51d0fe12b3193659e0eafe8eb3c45153c8e91
Tom Henderson <tomhenderson@mac.com>
2014-09-01 11:28:59 +1200

    Loop through sidebar images.

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,16 @@
 <div class="column images">
-	&nbsp;
+<?php
+
+if( class_exists('Dynamic_Featured_Image') ) {
+  global $dynamic_featured_image;
+  $featured_images = $dynamic_featured_image->get_featured_images( );
+
+  foreach ( $dynamic_featured_image as $featured_image ) {
+    ?>
+    <img src="<?=$dynamic_featured_image['full'];?>">
+    <?php
+  }
+}
+
+?>
 </div>
