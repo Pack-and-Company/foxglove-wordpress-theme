e019e27db396c737476ae4dcc07678a994040550
Tom Henderson <tomhenderson@mac.com>
2014-09-01 11:34:53 +1200

    Was looping through wrong variable.

--- sidebar.php
+++ sidebar.php
@@ -5,9 +5,9 @@ if( class_exists('Dynamic_Featured_Image') ) {
   global $dynamic_featured_image;
   $featured_images = $dynamic_featured_image->get_featured_images( );
 
-  foreach ( $dynamic_featured_image as $featured_image ) {
+  foreach ( $featured_images as $featured_image ) {
     ?>
-    <img src="<?=$dynamic_featured_image['full'];?>">
+    <img src="<?=$featured_image['full'];?>">
     <?php
   }
 }
