b05f2b78ea2a35bcb67ea925a17f4cbe739e6a84
Tom Henderson <tomhenderson@mac.com>
2014-09-01 16:27:41 +1200

    Add contact link to footer.

--- footer.php
+++ footer.php
@@ -1,12 +1,17 @@
 <div class="bg-footer">
   <div class="wrapper">
-    <div class="footer">
-      <p>
-        33 Queens Wharf, Wellington<br />
-        Postal Address: PO Box 25408, Panama St, Wellington 6146 <br />
-        Phone: <strong>04 460 9410</strong> | email: <a href="mailto:info@foxglovebar.co.nz">info@foxglovebar.co.nz</a><br />
-        For all function enquires <a href="#">please fill out our online form</a>.
-      </p>
+    <div class="footer two-columns">
+      <div class="column details">
+        <p>
+          33 Queens Wharf, Wellington<br />
+          Postal Address: PO Box 25408, Panama St, Wellington 6146 <br />
+          Phone: <strong>04 460 9410</strong> | email: <a href="mailto:info@foxglovebar.co.nz">info@foxglovebar.co.nz</a><br />
+          For all function enquires <a href="#">please fill out our online form</a>.
+        </p>
+      </div>
+      <div class="column">
+        <a class="contact-link" href="<?=get_home_url();?>/contact">VIEW OUR <strong>LOCATION MAP</strong></a>
+      </div>
     </div>
   </div>
 </div>
