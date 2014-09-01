9ff96a223fd87e4285d7fdc9bdcb1ea05425eff6
Tom Henderson <tomhenderson@mac.com>
2014-09-01 17:09:52 +1200

    Check if there are any images.

--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,7 @@ if( class_exists('Dynamic_Featured_Image') ) {
   global $dynamic_featured_image;
   $featured_images = $dynamic_featured_image->get_featured_images( );
 
-  if ( count($featured_images) > 0 ) {
+  if ( count($featured_images) > 1 ) {
 	  foreach ( $featured_images as $featured_image ) {
 	    ?>
 	    <img src="<?=$featured_image['full'];?>">
