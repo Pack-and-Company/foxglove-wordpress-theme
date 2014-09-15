f9a56c17ff02dd2a525755d83b5a7192420cf4b6
Tom Henderson <tomhenderson@mac.com>
2014-09-16 11:11:30 +1200

    Only show postal address if filled in.

--- footer.php
+++ footer.php
@@ -4,7 +4,12 @@
       <div class="column details">
         <p>
           <?=get_theme_mod('_street_address', '');?><br />
-          Postal Address: <?=get_theme_mod('_postal_address', '');?><br />
+          <?php
+            $postal_address = get_theme_mod('_postal_address', '');
+            if ($postal_address != '') {
+              echo "Postal Address:" + $postal_address;
+            }
+          ?>
           Phone: <strong><?=get_theme_mod('_phone_number', '');?></strong> | email: <a href="<?=get_theme_mod('_email_address', '#');?>"><?=get_theme_mod('_email_address', '');?></a><br />
           For all function enquires <a href="#">please fill out our online form</a>.
         </p>
