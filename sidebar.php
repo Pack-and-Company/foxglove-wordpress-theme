620ab2c9244d88f15590aedd64ed5c25ecee89d0
Tom Henderson <tomhenderson@mac.com>
2014-09-17 11:37:27 +1200

    Remove testing.

--- sidebar.php
+++ sidebar.php
@@ -6,8 +6,6 @@ if( class_exists('Dynamic_Featured_Image') ) {
   global $dynamic_featured_image;
   $featured_images = $dynamic_featured_image->get_featured_images( );
 
-  print_r($featured_images);
-
   if ( count($featured_images) > 0 ) {
 	  foreach ( $featured_images as $featured_image ) {
 	    ?>
